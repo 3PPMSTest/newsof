5bf7c9dff9352bf7cfce097f6ec145c9f8d17c1e
Test user <105701412+3PPMSTest@users.noreply.github.com>
2023-05-04 11:46:24 +0200

    Create APITestRunner.java

---
+++ APITestRunner.java
@@ -0,0 +1,14 @@
+package Automation;
+
+public class APITestRunner {
+    public static Response response = null;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        // TestBase.beforeClass();
+    }
+
+ 
+
+    
+}
